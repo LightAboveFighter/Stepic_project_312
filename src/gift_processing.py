92e8c66c72f67309db8a47e508bdd22691f2ab52
Ivan Gousev <vanyagousev@gmail.com>
2024-02-20 19:39:17 +0300

    structure declaration

--- src/gift_processing.py
+++ src/gift_processing.py
@@ -7,18 +7,24 @@ STEPIK_name_types = {"MultipleChoiceCheckbox()": "choice",
                      "MultipleChoiceRadio()": "choice",
                     }
 
-
+def __question_validator__(x:dict) -> bool:
+    '''returns True if question is valid'''
+    pass
 
 def __get_question_options__(x: gifpars.gift.Question) -> dict:
-    """get options from question in Stepik json format"""
+    """gets options dict for Stepik json"""
     pass
 
 def __get_question_data__(x: gifpars.gift.Question) -> dict:
+    """gets block dict for Stepik json"""
     pass
 
 def get_gift_dicts(filename: str) -> list:
+    """returns list if block dicts of question from GIFT file"""
     pass
 
+def get_Step_list(lesson_id: int) -> list:
+    pass
 
 if __name__ == "__main__":
     pass
