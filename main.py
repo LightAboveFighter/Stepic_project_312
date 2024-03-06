66c4a79b62c1f4f4f4cb6c74724804c90bca784d
Alexunder <skosarigin+github@gmail.com>
2024-03-06 11:17:58 +0300

    06.03.2024 small tests' fixes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import OAuthSession as auth
     по следующей ссылке: https://stepik.org/oauth2/applications/ (client type = confidential, authorization grant type = client credentials)
     Свой client_id и client_secret нельзя публиковать, поэтому я использовал input()
 """
-# После первого удачного теста можете закомментировать эти строки
+# После первого удачного тестирования можете закомментировать эти строки
 client_id = input("Client_id: ")            # Для pytest поставьте сюда свой id и secret, но потом не забудьте стереть эти значения
 client_secret = input("Client_secret: ")    # OAuthSession создает по заданным id и secret файл с их значениями
                                             # В последующие разы эти значения можно брать оттуда, файл добавлен в .gitignore
