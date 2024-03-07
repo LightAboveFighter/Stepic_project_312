8db209f140b082b6b05756a1dc8de335292fd4ad
Alexunder <skosarigin+github@gmail.com>
2024-03-07 23:38:59 +0300

    test_fixes

--- conftest.py
+++ conftest.py
@@ -5,18 +5,19 @@ import OAuthSession as auth
 
 @pytest.fixture(autouse=True, scope="session")
 def coverage():
+
     if not os.path.exists("Client_information.yaml"):
         auth.OAuthSession(input("Client_id: "), input("Client_secret: "))
     yield
-    # if os.path.exists("Check_file.yaml"):
-    #     os.remove("Check_file.yaml")
+
     if os.path.exists("Test course's title.yaml"):
         course = co.Course()
         course.load_from_file("Test course's title.yaml")
-        course.auth(auth.OAuthSession())
-        course.delete_network_course()
-        for j in range( len( course.structure["Course"]["Sections"])):
-            for i in range( len( course.structure["Course"]["Sections"][j]["Lessons"] )):
-                course.delete_network_lesson(j, i)
+        if not course.structure["Course"]["id"] == None:
+            course.auth(auth.OAuthSession())
+            course.delete_network_course()
+            for j in range( len( course.structure["Course"]["Sections"])):
+                for i in range( len( course.structure["Course"]["Sections"][j]["Lessons"] )):
+                    course.delete_network_lesson(j, i)
         os.remove("Test course's title.yaml")
 
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import logs.project_logger as log
 """ Для корректной работы моих функций вам нужно создать свое Степик API приложение
     по следующей ссылке: https://stepik.org/oauth2/applications/ (client type = confidential, authorization grant type = client credentials)
     Далее запускайте pytest с аргументом -s для первого раза
+    pytest -m local  -  запустит тесты функций не требующих подключение к интернету
+    pytest -m network  -  запустит тесты требующие подключение к интернету
 """
 
 log.activate_logger("D")
