1a5db369d623188b493185a84f59baaa4d1b359a
Mox <mox.dev@yandex.ru>
2024-03-22 22:44:59 +0300

    Rename constants

--- src/markdown_parsing.py
+++ src/markdown_parsing.py
@@ -1,20 +1,22 @@
 from abc import ABC
 import pyparsing as pp
+import src.API.Classes as sac
 import io
 
 
 class Step(ABC):
-    '''lesson_id: int
+    '''id: int
     step_name: str
-    step_info: str'''
-    
+    step_info: str
+    params: dict'''
+
     def __init__(
         self,
-        les_id: int,
+        id: int,
         nam: str,
         inf: list
     ) -> None:
-        self.lesson_id = les_id
+        self.id = id
         self.step_name = nam
         self.step_info = inf
 
@@ -25,9 +27,11 @@ class StepText(Step):
 
 
 class Lesson:
-    '''lesson_name: str
-    lesson_id: int
-    step_list: list'''
+    '''title: str
+    steps: list or []
+    id: int
+    sect_ids: list or []
+    params: dict'''
 
     def _module_step(self):
             step_type = pp.one_of(['QUIZ', 'CHOICE', 'TEXT'], as_keyword=True) ('type')
@@ -39,23 +43,23 @@ class Lesson:
         name = pp.rest_of_line ('name')
         parse_name = pp.Suppress(pp.Keyword('#') + pp.ZeroOrMore(pp.White())) + pp.Optional(name)
 
-        les_id = pp.Word(pp.nums) ('id')
-        parse_id = pp.Suppress(pp.Keyword('lesson') + pp.ZeroOrMore(pp.White()) + '=' + pp.ZeroOrMore(pp.White())) + les_id
-        
+        id = pp.Word(pp.nums) ('id')
+        parse_id = pp.Suppress(pp.Keyword('lesson') + pp.ZeroOrMore(pp.White()) + '=' + pp.ZeroOrMore(pp.White())) + id
+
         parse_step = self._module_step()
 
         with io.open(lesson_path, 'r', encoding='utf-8') as f:
             # Writting down name of the lesson
-            self.lesson_name = (parse_name.parseString(f.readline())).name
+            self.title = (parse_name.parseString(f.readline())).name
             
             # Writting down id of the lesson 
             for id_line in f:
                 if id_line != pp.Empty():
-                    self.lesson_id = (parse_id.parseString(id_line)).id
+                    self.id = (parse_id.parseString(id_line)).id
                     break
             
             # Writting down steps of the lesson
-            self.step_list = []
+            self = []
 
             step_lines = []
             first_cycle = True
@@ -64,10 +68,11 @@ class Lesson:
                 try:
                     new_step = parse_step.parseString(line)
                     if not first_cycle:
-                        self.step_list.append(StepText(self.lesson_id, previous_step.name, step_lines))  # for now -- only StepText
+                        self.steps.append(StepText(self.id, previous_step.name, step_lines))  # for now -- only StepText
                     step_lines = []
                     first_cycle = False
                     previous_step = new_step
                 except pp.ParseException:
                     step_lines.append(line)
-            self.step_list.append(StepText(self.lesson_id, previous_step.name, step_lines))
+            self.steps.append(StepText(self.id, previous_step.name, step_lines))
+        # self.API = sac.Lesson(self.title, self.id, self.steps)     # here may be params, which are totally optional (^人^)
