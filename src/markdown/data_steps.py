eec15de60b1073d59d68a2c630a4186ed743d2d4
Mox <mox.dev@yandex.ru>
2024-04-15 00:34:50 +0300

    Added step type QUIZ (improvements needed for choice and quiz variants)

--- src/markdown/data_steps.py
+++ src/markdown/data_steps.py
@@ -30,12 +30,6 @@ class DataStepChoice(DataStep):
     '''text: list[str]
     variants: list[variant_data]
     step_addons: dict'''
-    # option 
-    # {
-    #   text 
-    #   is_correct
-    # feedback
-    # }
 
     class variant:
         def __init__(self, text: str, is_correct: str, feedback: str = None):
@@ -47,27 +41,6 @@ class DataStepChoice(DataStep):
             else:
                 raise Exception('Undefined type of variant_data.')
             self.feedback = feedback
-
-    def add_variant(self, variant_data):
-        match (variant_data.type):
-            case '+':
-                self.correct[variant_data.value] = None
-            case '-':
-                self.incorrect[variant_data.value] = None
-            case _:
-                raise Exception('Undefined type of choice variant_data.')
-
-    def add_feedback(self, variant_data, feedback):
-        if feedback.type != 'HINT':
-            raise Exception('Wrong feedback type.')
-
-        match (variant_data.type):
-            case '+':
-                self.correct[variant_data.value] = feedback.value
-            case '-':
-                self.incorrect[variant_data.value] = feedback.value
-            case _:
-                raise Exception('Undefined type of choice variant_data.')
 
     def add_info(self, lines: list[str]):
         self.text = []
@@ -82,7 +55,6 @@ class DataStepChoice(DataStep):
             if getting_text:
                 try:
                     variant_data = ParsingModuleSchema.choice_variant().parseString(l)
-                    # add_variant
                     self.variants.append(DataStepChoice.variant(variant_data.value, variant_data.type))
                     getting_text = False
                     getting_variants = True
@@ -99,12 +71,10 @@ class DataStepChoice(DataStep):
                         getting_variants = False
                         getting_end = True
                         continue
-                    # self.add_feedback(variant_data, feedback)
                     self.variants[-1].feedback = feedback.value
                 except pp.ParseException:
                     if l != pp.Empty():
                         variant_data = ParsingModuleSchema.choice_variant().parseString(l)
-                        # self.add_variant(variant_data)
                         self.variants.append(DataStepChoice.variant(variant_data.value, variant_data.type))
                     continue
                     
@@ -118,12 +88,78 @@ class DataStepChoice(DataStep):
 
 
 class DataStepQuiz(DataStep):
+    # text before answer variants
     '''text: list[str]
     variants: dict
     step_addons: dict'''
+
+    class variant:
+        def __init__(self, text: str, label: str, feedback: str = None):
+            self.text = text
+            self.label = label
+            self.feedback = feedback
+
     def add_info(self, lines: list[str]):
-        # Нужно будет отпарсить все строчки находящиеся в lines
-        pass
+        self.text = []
+        self.variants = []
+        self.step_addons = {'SHUFFLE' : 'true'}
+
+        begin = 'TEXTBEGIN'
+        end = 'TEXTEND'
+        only_text = False
+        getting_text = True
+        getting_variants = False
+        getting_end = False
+
+        for l in lines:
+
+            
+            # Нужно добавить вариант, когда текст обрамляется в TEXTBEGIN и TEXTEND
+            if getting_text:
+                if (not only_text) and (begin in l):
+                    if l.strip().index(begin) == 0:
+                        l = l[l.index(begin) + len(begin):].strip()
+                        only_text = True
+
+                if (only_text) and (end in l):
+                    self.text.append(l[:l.index(end)])
+                    l = l[l.index(end) + len(end):].strip()
+                    only_text = False                
+
+                try:
+                    if only_text:
+                        raise pp.ParseException('only_text == True')
+                    variant_data = ParsingModuleSchema.quiz_variant().parseString(l)  # проблемы с этим парсером
+                    self.variants.append(DataStepQuiz.variant(variant_data.value, variant_data.label))
+                    getting_text = False
+                    getting_variants = True
+                    continue
+                except pp.ParseException:
+                    self.text.append(l)
+                    continue
+                    
+            if getting_variants:
+                try:
+                    feedback = ParsingModuleSchema.body_addon().parseString(l)
+                    if feedback.type != 'HINT':
+                        self.step_addons[str(feedback.type)] = feedback.value
+                        getting_variants = False
+                        getting_end = True
+                        continue
+                    self.variants[-1].feedback = feedback.value
+                except pp.ParseException:
+                    if l != pp.Empty():
+                        variant_data = ParsingModuleSchema.quiz_variant().parseString(l)
+                        self.variants.append(DataStepQuiz.variant(variant_data.value, variant_data.label))
+                    continue
+                    
+            if getting_end:
+                try:
+                    addon = ParsingModuleSchema.body_addon().parseString(l)
+                    self.step_addons[str(addon.type)] = addon.value
+                    continue
+                except pp.ParseException:
+                    continue
 
 
 class DataStepCreationSchema():
