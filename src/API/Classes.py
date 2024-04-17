73faa3921e9a58c42a046b5d9e144442fb25a68c
Alexunder <skosarigin@gmail.com>
2024-04-17 21:25:28 +0300

    fix range quality

--- src/API/Classes.py
+++ src/API/Classes.py
@@ -64,8 +64,8 @@ class Lesson:
             self.id = id
             for i in self.steps:
                 i.lesson_id = self.id
-            for i in self.steps:
-                i.send(i, session)
+            for i in range(len(self.steps)):
+                self.steps[i].send(i, session)
 
         return request_status(r, 201)
 
@@ -361,13 +361,13 @@ class Section:
             if is_success(r, 201):
                 id = json.loads(r.text)["sections"][0]["id"]
                 self.id = id
-                for i in self.lessons:
-                    i.tie(self.id, i, session)
+                for i in range(len(self.lessons)):
+                    self.lessons[i].tie(self.id, i, session)
             return request_status(r, 201)
         
-        for i in len(self.lessons):
-            if self.lessons[i].is_tied(self.id):
-                self.lessons[i].tie(self.id, i, session)
+        for i in self.lessons:
+            if i.is_tied(self.id):
+                i.tie(self.id, i, session)
         return success_status(True, "Already sent, modify lessons")
     
     def send_lesson(self, les_pos: int, session: OAuthSession):
