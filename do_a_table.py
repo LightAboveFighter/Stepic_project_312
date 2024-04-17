72b75ce5e720d6a66339baaf1756c3b350cc3ad2
Lida <salnikova.lidiya.05@gmail.com>
2024-04-17 22:38:07 +0300

    The big table is done except beauty and uncoding

--- do_a_table.py
+++ do_a_table.py
@@ -1,26 +1,54 @@
 import yaml
-import information_for_table
+from information_for_table import Class
+import src.API.OAuthSession as auth
 
-with open('klass.yaml') as fh:
-    klass = yaml.load(fh, Loader=yaml.FullLoader)
-
-with open('lesson.yaml') as fh:
-    lessons = yaml.load(fh, Loader=yaml.FullLoader)
-
-print(klass[0])
-print(lessons[0])
-your_class_id = 0
+your_class_id = 56799
 t = Class(your_class_id, auth.OAuthSession())   #enter your_class_id and, if you need, args to OAuthSession
+#t.update_info_lessons()
 t.get_table()
+print(t.class_id, t.course_id)
+
+with open(f'klass_{t.class_id}.yaml', mode='r', encoding='utf-8') as fh:
+    info_klass = yaml.load(fh, Loader=yaml.FullLoader)  #klass grades
+
+with open(f'src/data/course_{t.course_id}.yaml', mode='r', encoding='utf-8') as fh:
+    info_course = yaml.load(fh, Loader=yaml.FullLoader) #course structure
 
 file_html = open("table1.html", "w")
-file_html.write("<!DOCTYPE html>\n<html>\n<head>\n<style>\ntable {\n  font-family: arial, sans-serif;\n  border-collapse: collapse;\n  width: 100%;\n}\n\ntd, th {\n  border: 1px solid #dddddd;\n  text-align: left;\n  padding: 8px;\n}\n\ntr:nth-child(even) {\n  background-color: #dddddd;\n}\n</style>\n</head>\n<body>\n\n")
-file_html.write(f"<h2>Класс id:{t.class_id}</h2>\n<h3>Курс id:{t.course_id}  Название курса:{t.course_name}</h3>\n\n<table>\n  <tr>\n    <th>Users</th>\n")
-for i in range(len(lessons)):
-    file_html.write(f"    <th>{lessons[i].get('lesson_id')}</th>\n")
+file_html.write("<!DOCTYPE html>\n<html>\n<head>\n<meta charset='UTF-8'>\n<style>\ntable {\n  font-family: arial, sans-serif;\n  border-collapse: collapse;\n  width: 100%;\n}\n\ntd, th {\n  border: 1px solid #dddddd;\n  text-align: left;\n  padding: 8px;\n}\n\ntr:nth-child(even) {\n  background-color: #dddddd;\n}\n</style>\n</head>\n<body>\n\n")
+file_html.write(f"<h2>Класс id:{t.class_id}</h2>\n  <h3>Курс id:{t.course_id}  Название курса:{t.course_name}</h3>\n\n  <table>\n  <tr>\n    <th>Id</th>\n    <th>Имя</th>\n    <th>Всего</th>\n")
+
+section_structure = []
+for section in info_course["Course"]["sections"]:
+    file_html.write(f"    <th>{section['title']}</th>\n")
+    steps_of_section = []
+    for lesson in section["lessons"]:
+            lesson_id = lesson["id"]
+            for step in range(len(lesson["steps"])):
+                try:
+                    name_step = str(lesson_id)+"-"+str(step+1)
+                    if info_klass[0][name_step]:
+                        steps_of_section.append(name_step)
+                except:
+                    pass
+    section_structure.append(steps_of_section)
 file_html.write(f"  </tr>\n  <tr>\n")
-for i in range(len(klass)):
-    file_html.write(f"  <tr>\n    <td>{klass[i].get('name')}</td>\n    <td>{klass[i].get('Result_lesson1')}</td>\n    <td>{klass[i].get('Result_lesson2')}</td>\n    <td>{klass[i].get('Result_lesson3')}</td>\n  </tr>\n")
+
+for student in range(len(info_klass)):
+    course_result = 0
+    file_html.write(f"  <tr>\n    <td>{info_klass[student].get('id')}</td>\n    <td>{info_klass[student].get('name')}</td>\n")
+    results_one_student = ""
+    for section in range(len(info_course["Course"]["sections"])):
+        section_result = 0
+        for step in section_structure[section]:
+            try:
+                section_result += info_klass[student][str(step)]["score"]
+            except:
+                pass
+        course_result += section_result        
+        results_one_student += f"    <td>{int(section_result)}</td>\n"
+    file_html.write(f"    <td>{int(course_result)}</td>\n" + results_one_student)
+    file_html.write(f"</tr>\n")
 
 
 file_html.write("</table>\n\n</body>\n</html>")
--- information_for_table.py
+++ information_for_table.py
@@ -26,9 +26,8 @@ class Class:
         self.course_name = ""
         self.class_id = class_id
         self.student_ids = self.get_student_ids()
-        self.student_names = self.get_names()
+        self.student_names_and_ids = self.get_names()
         self.course_id = self.get_course_id()
-        self.sections_lessons_steps = self.update_info_lessons()
 
     def get_student_ids(self):  #creates the list of students' ids 
         """Student list in class class_id."""
@@ -52,10 +51,10 @@ class Class:
         api = api[:-1]
         r = requests.get(api, headers=self.session.headers())
         work = r.json()
-        names_list = []
+        names_and_ids = {}
         for user in work["users"]:
-            names_list.append(user["full_name"])
-        return names_list
+            names_and_ids.update({user["id"]: user["full_name"]})
+        return names_and_ids
 
     def get_course_id(self):    #finds course_id by class_id
         api = f"https://stepik.org/api/classes/{self.class_id}"
@@ -65,20 +64,20 @@ class Class:
 
     def update_info_lessons(self):  #returns all information about structure of course(sections/lessons/steps)
         a = cl.Course()
-        yaml_name = f"{self.course_id}.yaml"
+        yaml_name = f"course_{self.course_id}.yaml"
         if os.path.exists(yaml_name):
             a.load_from_file(yaml_name)
         else:
-            a.load_from_net(self.course_id, False, self.session)
-            a.save(yaml_name)
-        self.course_name = a.title()
+            a.load_from_net(self.course_id, sourse=False, session=self.session)
+            a.save(filename=yaml_name[:-5]) #Саня дописывает ".yaml" сам. Не знаю зачем
+        self.course_name = a.title
         d = a.dict_info()
         lessons_ids = []
-        for i in d["Sections"]:
-            section_ids = []
-            for j in i["Lessons"]:
-                section_ids.append(j["id"])
-            lessons_ids.append(section_ids)
+        for i in d["sections"]:
+            one_section_lesson_ids = []
+            for j in i["lessons"]:
+                one_section_lesson_ids.append(j["id"])
+            lessons_ids.append(one_section_lesson_ids)
         return lessons_ids
 
     def get_table(self):    #creates a yaml with scores, ids and names for every student
@@ -90,12 +89,11 @@ class Class:
         for student in course_grades:
             student_in_file = {}
             student_id = student["user"]
-            number_in_student_list = self.student_ids.index(student_id)
-            student_in_file["user"] = student_id
-            student_in_file["name"] = self.student_names[number_in_student_list]
+            student_in_file["id"] = student_id
+            student_in_file["name"] = self.student_names_and_ids.get(student_id)
             for i in student["results"]:
-                student_in_file[i] = {done: student["results"][i]["is_passed"], score: student["results"][i]["score"]}
+                student_in_file[i] = {"done": student["results"][i]["is_passed"], "score": student["results"][i]["score"]}
             klass_file.append(student_in_file)
-        with open("klass.yaml", "w") as file:
-            yaml.dump(klass_file, file, allow_unicode=True)
+        with open(f'klass_{self.class_id}.yaml', "w", encoding='utf-8') as file:
+            yaml.dump(klass_file, file, allow_unicode=True, encoding='utf-8')
             
